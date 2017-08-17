215bff8e1d4335bdb902684ea8464f6f03887d1f
Haojun Lu <haojunl@andrew.cmu.edu>
2017-08-17 20:10:31 +0800

    modify gen.sh add empty test case

---
+++ 9999-Temp/temp/Solution.java
@@ -0,0 +1,12 @@
+package temp;
+public class Solution {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		sol.test();
+	}
+	public void test() {
+		System.out.println("Test starts!");
+		System.out.println("Good Luck!");
+	}
+}
--- gen.sh
+++ gen.sh
@@ -1,3 +1,9 @@
+# try to generate sth useful in all problems
 #!/bin/bash
-echo //url:  "$1" > Solution.java
+mkdir -p "$1"/"$2"
+cd "$1"
+cd "$2"
+echo //url:  "$3" > Solution.java
+echo package "$2"";" >> Solution.java
+tail -n+2 ../../9999-Temp/temp/Solution.java >> Solution.java
 lolcat Solution.java
