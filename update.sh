cbc3fe1283fbce752208196674b52fdccbe85ced
Haojun Lu <haojunl@andrew.cmu.edu>
2017-08-17 21:43:50 +0800

    add empty line in update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 git status
+echo ""
+echo ""
+echo ""
 echo "Please enter commit log" | lolcat
 read log 
 echo ""
 echo $log | lolcat
+echo ""
+echo ""
 
 git add --all | lolcat
+echo ""
+echo ""
 git commit -m "$log" | lolcat
