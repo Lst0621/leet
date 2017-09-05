0265fc456229d9de62321ea2f8c6b294b75c2e3f
Haojun Lu <haojunlu@foxmail.com>
2017-09-05 02:53:11 -0400

    #417-WaterFlow# created file

---
+++ 417-WaterFlow/waterFlow/Solution.java
@@ -0,0 +1,19 @@
+//url: https://leetcode.com/problems/pacific-atlantic-water-flow/description/
+package waterFlow;
+public class Solution {
+
+	/** Ctrl + C starts here **/
+
+
+	/** Ctrl + C ends here **/
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		sol.test();
+	}
+	public void test() {
+		System.out.println("Test starts!");
+		System.out.println("Good Luck!");
+		System.out.println("");
+	}
+}
