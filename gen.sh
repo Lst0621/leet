3cc5476d4627c64a2c91417943343bdb2d756854
Haojun Lu <haojunlu@foxmail.com>
2017-09-05 02:48:31 -0400

    script for adding file to idea path?? took 2 hours, save 1 min each time called!!

--- gen.sh
+++ gen.sh
@@ -8,11 +8,11 @@ echo "Please enter url" | lolcat
 read problem
 
 mkdir -p "$foldname"/"$packname"
+
 cd "$foldname"
 cd "$packname"
 
 
-pwd | lolcat
 echo ""
 echo ""
 
@@ -25,4 +25,18 @@ echo "file created" | lolcat
 echo ""
 lolcat Solution.java
 echo ""
-pwd
+
+pwd | lolcat
+
+
+# add file path to ide
+cd ../
+cd ../
+
+foldpath="<sourceFolder url=\"file://\$MODULE_DIR$/"$foldname"\" isTestSource=\"false\" />"
+echo $foldpath
+line=$(grep "/content" leet.iml -n | cut -f1 -d':')
+sedcmd="$line i $foldpath"
+sed -i "$line i \      $foldpath" leet.iml
+
+
