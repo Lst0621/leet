6ca6caf750ed9108b42c41b7074a180e605893a4
Haojun Lu <haojunlu@foxmail.com>
2017-09-14 16:56:07 -0400

    #312-Balloon# dp: at least pass

--- 312-Balloon/balloon/Solution.java
+++ 312-Balloon/balloon/Solution.java
@@ -16,72 +16,41 @@ import java.util.List;
 public class Solution {
 	//fixme
 	/** Ctrl + C starts here **/
-	private HashMap<String,Integer> map;
+
+	int[][] temp;
 	public int maxCoins(int[] nums) {
 		int l = nums.length;
 		if(l==0) return 0;
-		map = new HashMap<>();
-		List<Integer> list = new LinkedList<>();
-		list.add(-1);
-		int sum = 0;
-		int temp = 1;
-		for(int i=0;i<l;i++){
-			int num = nums[i];
-			if(num>=1)
-				list.add(i);
-		}
-		list.add(-1);
-		int ret =  helper(list,nums);
+		temp = new int[l][l];
+		for(int i=0;i<l;i++)
+			for(int j=0;j<l;j++)
+				temp[i][j] = -1;
+		int ret =  helper(nums,0,l-1,1,1);
 		return ret;
 	}
 
-	private int helper(List<Integer> list,int[] nums){
+	private int helper(int[] nums,int start,int end,int a,int b){
+		//System.out.println(start +  " " + end);
+		if(end<start) return 0;
+		if(end==start) return a*b*fetch(nums,start);
 
-		StringBuilder sb= new StringBuilder();
-		for(int num:list){
-			sb.append(" ");
-			sb.append(num);
-		}
-		String str = sb.toString();
-		//System.out.println(str);
-
-		if(map.get(str)!=null) {
-			//System.out.println("cache");
-			return map.get(str);
-		}
+		if(temp[start][end]!=-1)
+			return temp[start][end];
 		int ret = 0;
-		int s = list.size();
-		if(s==3)
-		{
-			ret = nums[list.get(1)];
-			map.put(str,ret);
-			return ret;
-		}
 
-
-
-		for(int i=1;i<s-1;i++){
-
-			int temp = nums[list.get(i)];
-			int indexHead = list.get(i-1);
-			int head = indexHead>=0? nums[indexHead]:1;
-			int indexTail = list.get(i+1);
-			int tail = indexTail>=0? nums[indexTail]:1;
-
-			int sum = temp*head*tail;
-			List<Integer> next = new ArrayList<>(list);
-			next.remove(i);
-
-			sum += helper(next,nums);
-			if(sum>ret)
+		for(int i=start;i<=end;i++){
+			int sum = a*b*fetch(nums,i) + helper(nums,start,i-1,a,fetch(nums,i))+helper(nums,i+1,end,fetch(nums,i),b);
+			if(ret<sum)
 				ret = sum;
-			//list.add(i,temp);
 		}
-
-		map.put(str,ret);
+		temp[start][end] = ret;
 		return ret;
+	}
 
-
+	private int fetch(int[] nums,int index){
+		if(index<0||index>=nums.length)
+			return 1;
+		return nums[index];
 	}
 
 
@@ -96,6 +65,6 @@ public class Solution {
 		System.out.println("Good Luck!");
 		System.out.println("");
 		System.out.println(maxCoins(new int[]{8,2,6,8,9,8,1,4,1,5,3,7,7,0,4,2,2,5}));
-		//System.out.println(maxCoins(new int[]{3,1,5,8}));
+		//System.out.println(maxCoins(new int[]{1,2}));
 	}
 }
